c5caf26babb96ddb0edeb1176dce1d7a4fc12c2c
EelaiWind <tony333ts@gail.com>
2016-09-18 00:19:48 +0800

    add camera example

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main() {
 	char c;
 	while(scanf("%c", &c)!=EOF) {
 		if(c=='c') {
-			system("gphoto2 --capture-image-and-download --filename");
+			system("gphoto2 --capture-image-and-download --filename snapshot.jpg");
 		}
 		else if (c=='q') break;
 	}
